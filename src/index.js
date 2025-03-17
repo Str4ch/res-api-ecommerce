b5e1f613f079410f033cdd57ff56884665e7cb2d
Str4ch <dog.fek54@gmail.com>
2025-03-17 11:52:16 +0100

    Middleware

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use((req, res, next)=>{
+    req.multiplication = 4*7;
+    next();
+
+})
+
 app.get("/", (req, res) =>{
-    res.send("Hello world!");
+    res.send(req.multiplication.toString());
 });
 
 app.listen(port, () => {
