497b81e1a7efc777f761dfad74d74df347472715
Str4ch <dog.fek54@gmail.com>
2025-03-17 15:10:42 +0100

    user's route

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) =>{
-    res.send("Hello world!");
-});
+
+const userRoutes = require("./routes/users");
+
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Welcome to my API ! e-commerce backed 🤳")
+})
+
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
---
+++ src/routes/users.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) =>{
+    res.send("Users page");
+});
+
+router.post("/", (req, res) =>{
+    // Get the data from the request
+    console.log(req.body);
+    const { firstName, email, password } = req.body;
+
+    res.json({
+    firstName,
+    email,
+    password,
+    _id: "randomId4567"
+    })
+});
+
+
+module.exports = router
